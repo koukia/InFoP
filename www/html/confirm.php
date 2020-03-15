47dfe84e11f46b3b9c225eca13e4e0a3b96e43fc
mori <tk011875@gmail.com>
2020-03-15 17:58:23 +0900

    確認画面で入力値を表示まで. close #2

---
+++ www/html/confirm.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+</head>
+<body>
+
+<?php
+  include('sanitizing.php');
+?>
+<?php
+  $category = $_POST['category'];//htmlspecialchars
+  $name = validate_name($_POST['name']);
+  $phone = $_POST['phone'];
+  $content = $_POST['cotent'];
+?>
+
+<div><h2>お問い合わせの内容確認</h2></div>
+<p>
+お問い合わせ内容はこちらで宜しいでしょうか？<br>
+よろしければ「送信する」ボタンを押して下さい．
+</p>
+
+<div>
+  <form action="/confirm.php" method="post">
+    <input type="hidden" name="name" value="<?php echo $name; ?>">
+    <div>
+      <div>
+        <label>件名</label>
+        <?php echo $category; ?>
+      </div>
+      <div>
+        <label>お名前</label>
+        <?php echo $name; ?>
+      </div>
+      <div>
+        <label>電話番号</label>
+        <?php echo $phone; ?>
+      </div>
+      <div>
+        <label>お問い合わせ内容</label>
+        <?php echo nl2br($content); ?>
+      </div>
+    </div>
+      <input type="button" value="内容を修正" onclick="history.back(-1)">
+      <button type="submit" name="submit">送信</button>
+    </form>
+</div>
+</body>
+</html>
--- www/html/index.php
+++ www/html/index.php
@@ -1,7 +1,4 @@
 <?php
 // Debug
-echo htmlspecialchars($_POST['category']);
-echo htmlspecialchars($_POST['name']);
-echo htmlspecialchars($_POST['phone']);
-echo htmlspecialchars($_POST['cotent']);
+
 ?>
