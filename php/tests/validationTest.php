2311955aace7f3c6534cb93fbb78467fe39b4cf9
mori <tk011875@gmail.com>
2020-03-17 02:37:26 +0900

    名前入力のテストコードを追加した.

--- php/tests/validationTest.php
+++ php/tests/validationTest.php
@@ -3,8 +3,15 @@ use PHPUnit\Framework\TestCase;
 require_once 'html/validation.php';
 
 class ValidationTest extends TestCase {
-    public function testReturnHello() {
+    public function testInputNameValidation() {
+        // 未入力
         $actual = InquiryForm\validate_name("");
         $this->assertEquals(InquiryForm\PLEASE_INPUT, $actual);
+        // ５０文字より長い
+        $actual = InquiryForm\validate_name("123456789012345678901234567890123456789012345678901");
+        $this->assertEquals(InquiryForm\PLEASE_INPUT_WITHIN_50CHAR, $actual);
+        // ５０文字以内
+        $actual = InquiryForm\validate_name("12345678901234567890123456789012345678901234567890");
+        $this->assertEquals("", $actual);
     }
 }
