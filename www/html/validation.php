98f45112770c19c57a7b80ee1f759cedb04e800d
mori <tk011875@gmail.com>
2020-03-16 18:30:34 +0900

    入力値の validation を追加した．

--- www/html/validation.php
+++ www/html/validation.php
@@ -8,21 +8,27 @@ function validate_name($name) {
   return "";
 }
 function validate_email($email) {
+    if (empty($email)) {
+        return "入力してください．";
+    } elseif (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)) {
+        return "メールアドレスが正しくありません．";
+    }
+    return "";
 }
 function validate_phone($phone) {
-  if (empty($phone)) {
-    return "入力してください．";
-  } elseif (true) {  //TODO:正規表現とかで確認
-    return "半角数字のみで記入ください．";
-  }
-  return "";
+    if (empty($phone)) {
+        return "入力してください．";
+    } elseif (!preg_match("/^[0-9]{10,11}$/", $email)) {
+        return "半角数字のみで記入ください．";
+    }
+    return "";
 }
 function validate_inquiry($inquiry) {
-  if (empty($inquiry)) {
-    return "入力してください．";
-  } elseif (mb_strlen($inquiry) > 1000) {
-    return "1000文字以下で記入ください．";
-  }
-  return "";
+    if (empty($inquiry)) {
+        return "入力してください．";
+    } elseif (mb_strlen($inquiry) > 1000) {
+        return "1,000文字以下で記入ください．";
+    }
+    return "";
 }
 ?>
